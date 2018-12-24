dabddf7b2534a863f94b42f1097f32539d1a8a0e
zhangyanhui <zhangyanhui@iwubida.com>
2018-12-24 14:20:43 +0800

    完成测试用例的编写

--- src/main/java/com/iwubida/demo/mockito/controllers/ShopController.java
+++ src/main/java/com/iwubida/demo/mockito/controllers/ShopController.java
@@ -18,22 +18,50 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShopController {
 
     @Autowired
-    private CustomerService customerService;
+    private ShopService shopService;
 
     @Autowired
-    private ShopService shopService;
+    private CustomerService customerService;
 
     /**
      * 查询店铺指定用户的交易概述
+     * <a href="http://localhost:8080/statistics/1/1">测试</a>
      *
      * @param shopId     店铺id
      * @param customerId 消费者id
      * @return 概述
      */
-    @GetMapping("trade-statistics/{shopId}/{customerId}")
-    public String getTradeStatistics(@PathVariable("shopId") Long shopId, @PathVariable("customerId") Long customerId) {
-        return "\n\t店铺：" + shopService.getShopName(shopId)
-                + "\n\t消费者：" + customerService.getCustomerName(customerId)
-                + "\n\tblablabla";
+    @GetMapping("statistics/{shopId}/{customerId}")
+    public String getShopCustomerStatistics(@PathVariable("shopId") Long shopId, @PathVariable("customerId") Long customerId) {
+        return this.getShopDesc(shopId) + this.getCustomerDesc(customerId) + this.getOtherDesc();
+    }
+
+    /**
+     * 获取店铺描述
+     *
+     * @param shopId 店铺id
+     * @return 店铺描述
+     */
+    public String getShopDesc(Long shopId) {
+        return "\n\t店铺：" + shopService.getShopName(shopId);
+    }
+
+    /**
+     * 获取消费者描述
+     *
+     * @param customerId 消费者id
+     * @return 消费者描述
+     */
+    public String getCustomerDesc(Long customerId) {
+        return "\n\t消费者：" + customerService.getCustomerName(customerId);
+    }
+
+    /**
+     * 获取其他描述
+     *
+     * @return 其他描述
+     */
+    public String getOtherDesc() {
+        return "\n\tblablabla";
     }
 }
---
+++ src/test/java/com/iwubida/demo/mockito/ShopControllerUnitTest.java
@@ -0,0 +1,101 @@
+package com.iwubida.demo.mockito;
+
+import com.iwubida.demo.mockito.controllers.ShopController;
+import com.iwubida.demo.mockito.services.CustomerService;
+import com.iwubida.demo.mockito.services.ShopService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Controller单元测试，不使用Spring容器
+ *
+ * @author Zhang Yanhui
+ * @date 2018/12/24 10:38
+ */
+
+@Slf4j
+@RunWith(MockitoJUnitRunner.class)
+public class ShopControllerUnitTest {
+
+    private static long testBeginTime;
+    private static int testCount;
+
+    /**
+     * 默认调用原有方法，除非mock某个方法
+     */
+    @Spy
+    private ShopService shopService;
+
+    /**
+     * stub所有方法，调用方法前必须mock
+     */
+    @Mock
+    private CustomerService customerService;
+
+    /**
+     * 注入依赖
+     */
+    @Spy
+    @InjectMocks
+    private ShopController shopController;
+
+    /**
+     * 所有测试前执行
+     */
+    @BeforeClass
+    public static void beforeAllTests() {
+        testBeginTime = System.currentTimeMillis();
+    }
+
+    /**
+     * 所有测试后执行
+     */
+    @AfterClass
+    public static void afterAllTests() {
+        long testEndTime = System.currentTimeMillis();
+        log.info("测试结束，共执行{}个用例，用时{}ms", testCount, testEndTime - testBeginTime);
+    }
+
+    /**
+     * 每个测试前执行
+     */
+    @Before
+    public void beforeEachTest() {
+        doReturn("店铺30").when(shopService).getShopName(1L);
+        when(customerService.getCustomerName(any())).thenReturn("消费者16");
+    }
+
+    /**
+     * 每个测试后执行
+     */
+    @After
+    public void afterEachTest() {
+        ++testCount;
+        verify(shopController, atLeastOnce()).getShopCustomerStatistics(anyLong(), anyLong());
+    }
+
+    @Test
+    public void getShopCustomerStatisticsTest_validInput() {
+        String result = shopController.getShopCustomerStatistics(1L, 1L);
+        assertEquals(result, "\n\t店铺：店铺30\n\t消费者：消费者16\n\tblablabla");
+        verify(customerService).getCustomerName(1L);
+        verify(shopService).getShopName(anyLong());
+    }
+
+    @Test
+    public void getShopCustomerStatisticsTest_mockShop() {
+        doReturn("\n\t店铺：店铺30").when(shopController).getShopDesc(anyLong());
+        String result = shopController.getShopCustomerStatistics(1L, 1L);
+        assertEquals(result, "\n\t店铺：店铺30\n\t消费者：消费者16\n\tblablabla");
+        verify(customerService).getCustomerName(1L);
+        verify(shopService, never()).getShopName(anyLong());
+    }
+}
